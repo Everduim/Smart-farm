65923dd3d008a52a4474e8dcda5a771f8971b0c6
nilo <nilo.calleja@bootcamp-upgrade.com>
2023-05-25 17:07:51 +0200

    arreglillo

--- Front/src/pages/Animales.jsx
+++ Front/src/pages/Animales.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import "../styles/Animales.scss";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
