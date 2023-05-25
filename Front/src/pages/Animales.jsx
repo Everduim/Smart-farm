93a62ce87a6fd1e812a3aafbde93428e8fa480c2
nilo <nilo.calleja@bootcamp-upgrade.com>
2023-05-25 16:44:57 +0200

    animales

--- Front/src/pages/Animales.jsx
+++ Front/src/pages/Animales.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import "../styles/Animales.scss"
+import React from "react";
+import "../styles/Animales.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Animales = () => {
-  return (
-    <div>Animales</div>
-  )
-}
+  const [animales, setAnimales] = useState([]);
+  useEffect(() => {
+    const getAnimales = async () => {
+      const response = await axios.get("http://localhost:8000/animales");
+      console.log(response);
+      setAnimales(response);
+    };
+    getAnimales();
+  }, []);
 
-export default Animales
+  return (
+    <>
+      <div className="container">
+        {animales.map((animal) => (
+          <div className="container_cards">
+            <Link key={animal._id} to={` /animales/${animal._id}`}>
+              <img src={animal.imagen}></img>
+            </Link>
+            <h3>{animal.nombre}</h3>
+            <p>{animal.sexo}</p>
+            <p>{animal.raza}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Animales;
--- Front/src/pages/AnimalesID.jsx
+++ Front/src/pages/AnimalesID.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import "../styles/AnimalesId.scss"
+import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const AnimalesID = () => {
+
+  const {id} = useParams();
+  const[animal, setAnimal] = useState();
+
+  const getAnimal = async (id) => {
+    const resultado = await axios.get("http://localhost:3000/animales/" + id);
+    setAnimal(resultado.data);
+  }
+
+  useEffect(() => {
+    getAnimal(id);
+  }, []);
+
   return (
-    <div>AnimalesID</div>
+    <>
+
+      {animal && (
+        <div className='container_animalId'>
+            <h3>{animal.numId}</h3>
+            <ul className='ul_container'>
+              <li>{animal.especie}</li>
+              <li>{animal.edad}</li>
+              <li>{animal.peso}</li>
+              <li>{animal.salud}</li>
+            </ul>
+        </div>
+      )}
+    </>
   )
 }
 
