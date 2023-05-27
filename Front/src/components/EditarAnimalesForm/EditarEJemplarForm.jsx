647a0f9c6ac9a631466c7d9802dd36f2f974718b
eavellanet <eavellanet12@gmail.com>
2023-05-27 19:40:57 +0200

    modificaciones registro Ejemplar

--- Front/src/components/EditarAnimalesForm/EditarEJemplarForm.jsx
+++ Front/src/components/EditarAnimalesForm/EditarEJemplarForm.jsx
@@ -1,13 +1,14 @@
 // import "../styles/AnimalesId.scss";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { JwtContext } from "../../context/jwtContext";
+import "./EditarEjemplarForm.scss"
 
 const EditarEjemplarForm = () => {
   const { id } = useParams();
   const [ejemplar, setEjemplar] = useState(null);
-  const {token} = useContext(JwtContext)
+  const { token } = useContext(JwtContext)
   console.log(id)
 
   const getEjemplar = async (id) => {
@@ -22,28 +23,60 @@ const EditarEjemplarForm = () => {
     getEjemplar(id);
   }, []);
 
+  async function tryActualizar(ejemplar) {
+    try {
+      await axios.put(`http://localhost:4000/ejemplar/${ejemplar._id}`,ejemplar)
+      setEjemplar(null);
+    } catch (e) {
+      console.log(e)
+    }
+
+  }
+
 
   return (
     <>
-           {ejemplar !== null && (
-        <div key={ejemplar._id} className="container_animalId">
-          <h3>{ejemplar.identificador}</h3>
-          <ul className="ul_container">
-            <li>
-              {ejemplar.especie.nombre} - {ejemplar.especie.raza} -{" "}
-              {ejemplar.especie.sexo}
-            </li>
-            <li>{ejemplar.edad}</li>
-            <li>{ejemplar.peso}</li>
-            <li>{ejemplar.salud}</li>
-            <img src={ejemplar.especie.imagen} alt="" />
-            <li>
-              <small>{ejemplar._id}</small>
-            </li>
-          </ul>
-          <Link to={`/animalesid/${ejemplar.especie._id}`}>
-            <button type="submit"></button>
-          </Link>
+      
+      {ejemplar !== null && (
+        <div key={ejemplar._id} className="container_ejemplar">
+          <form className="animal_form">
+            <img className="imagen_a" src={ejemplar.especie.imagen} alt="" />
+            <ul className="inputs-ul">
+              <li className="input-li">
+                <small>Identificador:</small>
+                <input value={ejemplar.identificador}  onChange={(e) =>
+                    setEjemplar({ ...ejemplar, identificador: e.target.value })
+                  }></input>
+              </li>
+              <li>
+                <small>Edad: </small>
+                <input value={ejemplar.edad}  onChange={(e) =>
+                    setEjemplar({ ...ejemplar, edad: e.target.value })
+                  } ></input>
+              </li>
+              <li>
+                <small>Peso: </small>
+                <input value={ejemplar.peso} onChange={(e) =>
+                    setEjemplar({ ...ejemplar, peso: e.target.value })
+                  }></input>
+              </li>
+              <li>
+                <small>Salud: </small>
+                <input value={ejemplar.salud}  onChange={(e) =>
+                    setEjemplar({ ...ejemplar, salud: e.target.value })
+                  }></input>
+              </li>
+
+
+              <li>
+                {ejemplar.especie.nombre} - {ejemplar.especie.raza} -{" "}
+                {ejemplar.especie.sexo}
+              </li>
+            </ul>
+
+            <button onClick={() => tryActualizar(ejemplar)}>Actualizar</button>
+          </form>
+
         </div>
       )}
 
